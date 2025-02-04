a871ead4f21f741f63e0f9c2019679a40f6aecf6
johnpaul <dexcripter@gmail.com>
2025-02-04 22:26:03 +0100

    the mint fn

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -13,18 +13,34 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 ///      This contract is just the ERC20 implementation of our stablecoin system
 
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
-
     error DecentralizedStableCoin__NotEnoughToken();
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__CannotMintZeroTokens();
+    error DecentralizedStableCoin__NotZeroAddress();
 
     constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
         if (balance <= 0) revert DecentralizedStableCoin__NotEnoughToken();
-        if (balance < _amount) revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        if (balance < _amount)
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
 
-        // basically, this means go to the user class (the overridden function) and use their brun implementation. 
-        super.burn(_amount); 
+        // basically, this means go to the user class (the overridden function) and use their brun implementation.
+        super.burn(_amount);
+    }
+
+    // There is not `mint` function in the ERC20Burnable.sol, but there is a _mint
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) revert DecentralizedStableCoin__NotZeroAddress();
+        if (_amount <= 0)
+            revert DecentralizedStableCoin__CannotMintZeroTokens();
+
+        // I'm not overriding any function, so theres no need to call the `super` keyword.
+        _mint(_to, _amount);
+        return true;
     }
 }
