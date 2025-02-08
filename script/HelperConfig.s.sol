3bcac8d4833b7db79e500ab16c05dbe23b213d7e
johnpaul <dexcripter@gmail.com>
2025-02-08 21:42:18 +0100

    cp

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        address wethUSDPriceFeed;
+        address wbtcUSDPriceFeed;
+        address weth;
+        address wbtc;
+        uint256 deployerKey;
+    }
+
+    NetworkConfig public activeNtwkConfig;
+
+    constructor() {}
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        return NetworkConfig({
+            wethUSDPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
+            wbtcUSDPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+    }
+
+    function getOrCreateAnvilConfig() public returns (NetworkConfig memory) {
+        if (activeNtwkConfig.wethUSDPriceFeed != address(0)) {
+            return activeNtwkConfig;
+        }
+        // return NetworkConfig()
+    }
+}
